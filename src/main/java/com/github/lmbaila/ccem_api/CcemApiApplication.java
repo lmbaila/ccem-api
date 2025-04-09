06ed93eede0edabd26aef55e58e039cf8ed52c5d
Lazaro Mbaila <lazarozito@gmail.com>
2025-04-09 20:48:00 +0200

    first commit

---
+++ src/main/java/com/github/lmbaila/ccem_api/CcemApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.lmbaila.ccem_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcemApiApplication.class, args);
+	}
+
+}
